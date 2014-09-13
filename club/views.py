f65fda32e0d6301ad720f729e9e32197988061f3
Aaron Pardes <aaronpardes@gmail.com>
2014-09-13 17:40:32 -0400

    still testing

--- club/views.py
+++ club/views.py
@@ -23,9 +23,9 @@ def chargeCredit(request):
 from django_twilio.decorators import twilio_view
 from twilio.twiml import Response
  
-@twilio_view
+@csrf_exempt
 def sms(request):
     r = Response()
     r.message('Hello from your Django app!')
-    return r
+    return HttpResponse(r.toxml(), content_type='text/xml')
 
