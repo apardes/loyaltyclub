d76bf3745a8f75be98be408ece8afd637b6e82fa
Aaron Pardes <aaronpardes@gmail.com>
2014-09-13 16:19:52 -0400

    improper import

--- loyaltyclub/urls.py
+++ loyaltyclub/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from tastypie.api import Api
-from api import BusinessResource, CreditResource, TransactionResource
+from api import BusinessResource, TransactionResource
 
 
 
